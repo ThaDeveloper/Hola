a2afe1eedd32f1a322ecf75f195d05c93e215538
Justin Ndwiga <ndwigajustin@gmail.com>
2017-07-02 09:18:17 +0000

    Displaying Posts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
   end
 
   def home
+    @posts = Post.all
   end
 
   def profile
@@ -14,8 +15,10 @@ class PagesController < ApplicationController
       #redirect to 400(root for now)
       redirect_to root_path, :notice=>  "Username not found"
     end
+    @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
   end
 
   def explore
+    @posts = Post.all
   end
 end
