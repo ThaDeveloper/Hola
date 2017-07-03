010d853312f90c3464e13e88835f21f758467d6a
Justin Ndwiga <ndwigajustin@gmail.com>
2017-07-03 12:07:17 +0000

    Post/Tweet form

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+    
+    def new
+        @post = Post.new
+    end
+    
+    def create
+        @post = Post.new(post_params)
+        @post.user_id = current_user.id
+        respond_to do |f|
+            if(@post.save)
+                f.html{ redirect_to "", notice: "Tweet saved"}
+            else
+                f.html{ redirect_to "", notice: "Error:Tweet not saved"}
+            end
+        end
+    end
+    
+    private
+    def post_params# allows certaind data to be passed via form
+        
+       params.require(:post).permit(:user_id, :content)
+    
+    end
+end
